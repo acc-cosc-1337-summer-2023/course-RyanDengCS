7e62a8911133f6acd6de548b40732ea6aa9e7728
RyanDengCS <135080091+RyanDengCS@users.noreply.github.com>
2023-06-11 04:26:48 +0000

    modified:   src/homework/01_data_types/data_types.cpp 	modified:   src/homework/01_data_types/data_types.h 	modified:   src/homework/01_data_types/main.cpp 	modified:   test/homework/01_data_types/01_data_types_tests.cpp

--- src/homework/01_data_types/main.cpp
+++ src/homework/01_data_types/main.cpp
@@ -1,12 +1,27 @@
 ﻿//write include statements
-
+#include<iostream>
+#include<data_types.h>
 
 //write namespace using statement for cout
-
+using std::cout;//displays to screen
+using std::cin;//capture data from the keyboard
 
 int main()
 {
-	
+    //variables
+    int num;
+    int result;
+    int num1;
 
-	return 0;
+    //capture num and display
+    cout<<"Enter a number: ";
+    cin>>num;
+    result = multiply_numbers(num);
+    cout<<result<<"\n";
+
+    //call function with num1=4 and display
+    num1 = 4;
+    result = multiply_numbers(num1);
+	cout<<result<<"\n";
+    return 0;
 }
